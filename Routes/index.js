cc52b6f829a47c38084f3f6e85de1143905ed385
Keshav <keshavthakur.001@gmail.com>
2021-01-28 04:08:14 +0530

    Add animation and user input in enterHome.ejs

--- Routes/index.js
+++ Routes/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if(req.cookies.name == undefined)
     res.render('enterHome')
+    else 
+    res.send("HELLO")
+    
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const cookieParser = require("cookie-parser")
+
 
 app.set('view engine', 'ejs')
 
+app.use(express.static("public"))
+
+
+app.use(cookieParser())
+
 app.use(require('./Routes'))
+
+
 
 const PORT = process.env.PORT || 3000
 
